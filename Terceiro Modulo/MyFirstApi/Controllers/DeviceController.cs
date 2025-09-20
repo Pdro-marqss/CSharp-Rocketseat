300d08808b7abfb8690ee2e02e3b22f934a2ef23
Pedro Marques <63529686+Pdro-marqss@users.noreply.github.com>
2025-09-19 21:17:39 -0300

    [RAW] Anotação sobre Sealed em herança (M3-P3-A5)

--- Terceiro Modulo/MyFirstApi/Controllers/DeviceController.cs
+++ Terceiro Modulo/MyFirstApi/Controllers/DeviceController.cs
@@ -7,6 +7,7 @@ namespace MyFirstApi.Controllers;
 // - Quando queremos dar acesso a um metodo ou propriedade somente a classe e a quem é derivada dela, usamos o protected (diferente do private que deixa somente a classe base utilizar)
 // - Overide significa uma sobre-escrita de um abstract derivado. Assim nos nao mudamos a funcao original da classe pai mas sim criamos uma copia e sobrescrevemos.
 // - Virtual diz que a classe derivada tem permissao de reescrever esse metodo. 
+// - Podemos marcar uma classe como sealed, significa que essa classe NAO pode mais servir de base para nenhuma outra. Assim nao teram derivadas dela
 
 // Comentei os 2 abaixo porque agora o controller herda (é derivado) de um controller base (um pai). E por isso herda dele essas duas propriedades
 // [Route("api/[controller]")]
